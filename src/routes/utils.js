acaadcdfe08eb8c8b3f8cfe0c6addbb40aa58ca3
irfanismail <irfan.ismail96@gmail.com>
2022-04-17 06:34:57 +0800

    fix: jsdoc makeRoute

--- src/routes/utils.js
+++ src/routes/utils.js
@@ -3,8 +3,8 @@
  * @param {string} path URL path
  * @param {ReactNode} component Page component
  * @param {string} name Route namespace
- * @param {ReactNode} layout Layout component
- * @returns
+ * @param {Array<ReactNode>|ReactNode} layout Layout component
+ * @returns {Object} Route object
  */
 export const makeRoute = (path, name, component, layout = null) => {
     if (!path || !component || !name) throw new Error('Path, component and name are required')
